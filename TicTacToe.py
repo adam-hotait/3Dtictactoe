c90e3d3c6da110a068e7961cce6c7e4b381cea06
adam-hotait <adam.hotait@student.ecp.fr>
2018-10-06 15:18:18 +0200

    Removed blank lines

--- TicTacToe.py
+++ TicTacToe.py
@@ -9,24 +9,18 @@ class TicTatToeMain()
 		"""Constructor"""
 		
 		self.__window_dimension = (1280, 720)
-		
 		self.__window = Window(self.__window_dimension)
 		
 		running = True
 		
 		while running:
-			
 			menu = Menu(self.__window)
-			
 			choice = menu.get_choice()
 			
 			if choice == "QUIT":
 				running = False
 			
 			elif choice == "JOIN":
-				
 				host, port = menu.get_server()
-				
 				client = Client(self.__window)
-				
 				client.join_server()
