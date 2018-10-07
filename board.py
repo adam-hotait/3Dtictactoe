5158cca504fbf2e9d7cb1f038fb1cd0babb720e7
adam-hotait <adam.hotait@student.ecp.fr>
2018-10-07 23:46:10 +0200

    Minor refactoring and comments

--- board.py
+++ board.py
@@ -14,7 +14,8 @@ class Board:
         if self.board[i][j][k] != 0:
             raise Exception("Occupied space")
         else:
-            self.check_for_win_pos(i, j, k, player_id)
+            # Checking for winning position before adding token on board to prevent zero division
+            self.check_for_win_pos(i, j, k,player_id)
             self.board[i][j][k] = player_id
 
     def check_for_win_pos(self, i, j, k, player_id):
@@ -25,10 +26,10 @@ class Board:
         print(np.argwhere(self.board == player_id))
         print('vectors')
         print(vectors)
-        print('-'*11+'dot'+'-'*11)
+        print('-' * 11 + 'dot' + '-' * 11)
         for a in range(0, len(vectors)):
-            for b in range(a+1, len(vectors)):
-                scalar_product = np.dot(vectors[a], vectors[b])\
+            for b in range(a + 1, len(vectors)):
+                scalar_product = np.dot(vectors[a], vectors[b]) \
                                  / (np.linalg.norm(vectors[a], 2) * np.linalg.norm(vectors[b], 2))
                 print(scalar_product, ' scalar product')
                 print(vectors[a], ' a')
