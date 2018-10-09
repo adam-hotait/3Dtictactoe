f67b18093c23794b562f9771cd1a53a4388ba9e4
adam-hotait <adam.hotait@student.ecp.fr>
2018-10-09 14:47:44 +0200

    Made check_win function return arg instead of print()

--- board.py
+++ board.py
@@ -15,34 +15,27 @@ class Board:
             raise Exception("Occupied space")
         else:
             # Checking for winning position before adding token on board to prevent zero division
-            self.check_for_win_pos(i, j, k,player_id)
+            print(self.check_for_win_pos(i, j, k, player_id))
             self.board[i][j][k] = player_id
 
     def check_for_win_pos(self, i, j, k, player_id):
-        print('-' * 11 + 'new token' + '-' * 11)
         new_token = np.array([i, j, k])
         vectors = (np.argwhere(self.board == player_id)) - new_token
-        print('Board')
-        print(np.argwhere(self.board == player_id))
-        print('vectors')
-        print(vectors)
-        print('-' * 11 + 'dot' + '-' * 11)
         for a in range(0, len(vectors)):
             for b in range(a + 1, len(vectors)):
                 scalar_product = np.dot(vectors[a], vectors[b]) \
                                  / (np.linalg.norm(vectors[a], 2) * np.linalg.norm(vectors[b], 2))
-                print(scalar_product, ' scalar product')
-                print(vectors[a], ' a')
-                print(vectors[b], 'b')
                 if np.isclose(scalar_product, 1) or np.isclose(scalar_product, -1):
-                    print('ok ', new_token, vectors[a] + new_token, vectors[b] + new_token)
-                else:
-                    print('no ', new_token)
+                    return True, new_token, vectors[a] + new_token, vectors[b] + new_token
+        return False, new_token
 
 
 if __name__ == "__main__":
     board = Board()
+    board.set_token(1, 1, 1, 1)
+    board.set_token(0, 0, 0, 2)
     board.set_token(1, 0, 1, 1)
-    board.set_token(2, 0, 0, 1)
-    board.set_token(2, 2, 0, 1)
-    board.set_token(2, 1, 0, 1)
+    board.set_token(1, 2, 1, 2)
+    board.set_token(1, 0, 0, 1)
+    board.set_token(1, 0, 2, 2)
+    board.set_token(1, 2, 2, 1)
